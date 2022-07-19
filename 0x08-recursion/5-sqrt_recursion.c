fbbe1aab3634863c906e0b850eb9ef438f271504
chinonsoobaji <chinonsoobaji@gmail.com>
2022-07-19 15:35:37 -0700

    A function that returns the natural square root of a number.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _alcuadrado - give me the square root of a
+ * @n: the given number
+ * @corre: from 0 up looking for square always less than the half of the number
+ * Return: if does not have square or corre if it find it
+ */
+
+int _alcuadrado(int corre, int n)
+{
+if (corre < 0)
+return (-1);
+if (corre == 1)
+return (1);
+if (n > corre / 2)
+return (-1);
+if (corre != n * n)
+return (_alcuadrado(corre, n + 1));
+if (corre == n * n)
+return (n);
+return (-1);
+}
