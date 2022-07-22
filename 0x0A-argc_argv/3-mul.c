e683ec749e5ade3430c900110e259295b5f85fcb
chinonsoobaji <chinonsoobaji@gmail.com>
2022-07-22 09:07:20 -0700

    Fix Tasks 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,18 +9,19 @@
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int x, y;
+int a, b, result;
 
-if (argc < 3)
+if (argc <= 2)
 {
-if (argc < 3)
+printf("Error\n");
 return (1);
 }
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-printf("%d\n", x * y);
-
+/* atoi convert a string to an int */
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+result = a * b;
+printf("%d\n", result);
 return (0);
 }
