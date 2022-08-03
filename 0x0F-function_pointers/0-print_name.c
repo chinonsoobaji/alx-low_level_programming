450bfb0a3a23a36c94c7736bcf8705a7f0892d89
chinonsoobaji <chinonsoobaji@gmail.com>
2022-08-03 13:51:09 -0700

    FIX 0,1,2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,12 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
-  * print_name - initializes a variable
-  *
-  * @name: name.
-  * @f: pointer.
-  * Return: nothing.
-  */
+ * print_name - prints a name.
+ * @name: the name
+ * @f: the function to print with
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
-	{
-		return;
-	}
-	if (f == NULL)
-	{
-		return;
-	}
-	f(name);
+	if (name && f)
+		f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,24 +1,18 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
-  * array_iterator - initializes a variable
-  *
-  * @array: name.
-  * @size: pointer.
-  *@action: pointer.
-  * Return: nothing.
-  */
+ * array_iterator - execute a function pointer on each element of an array
+ * @array: array of integers
+ * @size: size of array
+ * @action: function pointer
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array == NULL || size <= 0 || action == NULL)
-	{
+	if (!array || !action)
 		return;
-	}
+
 	for (i = 0; i < size; i++)
-	{
-		(*action)(array[i]);
-	}
+		action(array[i]);
 }
