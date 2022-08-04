6371fe389365e6a685a5910df5a44b03d0d853ca
chinonsoobaji <chinonsoobaji@gmail.com>
2022-08-04 14:27:53 -0700

    FIX 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,5 @@
-/*
- * File: 0-print_name.c
- * Auth: Brennan D Baraban
- */
-
 #include "function_pointers.h"
-
+#include <stio.h>
 /**
  * print_name - Prints a name.
  * @name: The name to be printed.
