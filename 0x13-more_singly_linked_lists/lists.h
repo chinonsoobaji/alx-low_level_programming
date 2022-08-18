4aae19d29bb6e9646f6ff6c13e5a450a146aa10b
chinonsoobaji <chinonsoobaji@gmail.com>
2022-08-18 01:17:58 -0700

    Completed Task

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON
-#define HOLBERTON
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * get_endianness -  function that checks the endianness.
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int a = 1;
+
+	a = a >> 1;
+	if (a != 0)
+		return (0);
+	return (1);
+}
